9a03b31f052b2357daab9b632340e86786b69538
andre-gicoski <andregicoskisz@gmail.com>
2025-03-18 11:57:56 -0300

    feat: implementing products_repository with tests

---
+++ src/models/sqlite/repository/products_repository_test.py
@@ -0,0 +1,22 @@
+from src.models.sqlite.settings.connection import SqliiteCOnnectionHandle
+from .products_repository import ProductsRepository
+import pytest
+
+conn_handle = SqliiteCOnnectionHandle()
+conn = conn_handle.connect()
+
+@pytest.mark.skip(reason="db interaction")
+def test_insert_products():
+    repo = ProductsRepository(conn)
+    name = "something"
+    price = "12.34"
+    quantity = 8
+    repo.insert_product(name, price, quantity)
+
+@pytest.mark.skip(reason="db interaction")
+def test_find_product():
+    repo = ProductsRepository(conn)
+    name = "something"
+    response = repo.find_product_by_name(name)
+    print (response)
+    print (type(response))
